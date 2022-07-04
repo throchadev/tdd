cefe4d069f6abba5adefdc67e8144fdf8beba16c
thiagorocha <thiago.rocha@soulisto.com.br>
2022-07-03 21:32:40 -0300

    code that works

--- src/main/java/br/com/throchadev/tdd/UserController.java
+++ src/main/java/br/com/throchadev/tdd/UserController.java
@@ -1,9 +1,21 @@
 package br.com.throchadev.tdd;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+  @GetMapping
+  public List<User> userList() {
+    List<User> userList = new ArrayList<>();
+    userList.add(new User("Thiago", "thiago@hotmail.com", 30));
+    return userList;
+  }
+
 }
